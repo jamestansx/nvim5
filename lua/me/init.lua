b39611bd7a1b4ed57071bbc88b893fcc9e4ae9f4
James Tan <jamestansx@gmail.com>
2024-12-06 16:23:07 +0800

    add note on disabling netrw

--- lua/me/init.lua
+++ lua/me/init.lua
@@ -11,6 +11,10 @@ vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_python3_provider = 0
 
+-- disable default plugins
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
+
 -- gloabl augroup
 augroup("global", { clear = true })
 
